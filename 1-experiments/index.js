bb923e86eb784117bab8c1ba307eeee3a8a000ba
apdonaghy <adonaghy@seattleu.edu>
2020-01-20 20:41:01 -0800

    pushing all fixes

--- 1-experiments/index.js
+++ 1-experiments/index.js
@@ -10,8 +10,6 @@ const NULL = null
 const NAN = NaN
 
 console.log("---- Use typeof operator ----");
-// TODO write typeof statements for all constants values following 
-// the final comma in the log statement
 console.log("STRING_VALUE",STRING_VALUE, "is of type", typeof STRING_VALUE);
 console.log("EMPTY_STRING_VALUE",EMPTY_STRING_VALUE, "is of type", typeof EMPTY_STRING_VALUE);
 console.log("INT_VALUE",INT_VALUE, "is of type",typeof INT_VALUE);
@@ -25,24 +23,14 @@ console.log("NAN",NAN, "is of type", typeof NAN);
 
 
 console.log("---- Test add operator with strings ----");
-//TODO implement the addition of types according the quoted portion of the log statement
-// log string + string
 console.log("string + string ", STRING_VALUE + STRING_VALUE);
-// log add string to int
 console.log("string + int ", STRING_VALUE + INT_VALUE);
-// log int + string
 console.log("int + string ", INT_VALUE + STRING_VALUE);
-// log int + int + string
 console.log("int + int + string ", INT_VALUE + INT_VALUE + STRING_VALUE);
-// log add string to float
 console.log("string + float ", STRING_VALUE + FLOAT_VALUE);
-// log add string to true
 console.log("string + true ", STRING_VALUE + BOOL_TRUE);
-// log add string to false
 console.log("string + false ", STRING_VALUE + BOOL_FALSE);
-// log add string to undefined
 console.log("string + undefined ", STRING_VALUE + UNDEFINED);
-// log add string to null
 console.log("string + null ", STRING_VALUE + NULL);
 
 console.log("---- assignment operator ----")
@@ -57,10 +45,6 @@ console.log("s1:", s1, "s2:",s2, "s3:", s3);
 
 console.log();
 
-// test truthy/falsy using == for string, empty string, null and undefined
-// use ternary operator to determine if the value would be treated as true or false
-
-//TODO add the ternary operation to see how JS converts non boolean types into truthy/falsy
 console.log("---- Test truthy/falsy using ternary operator ----");
 console.log("string is  ", STRING_VALUE ? true : false);
 console.log("int is ", INT_VALUE ? true : false);
--- 3-string-contains-hello/index.js
+++ 3-string-contains-hello/index.js
@@ -1,21 +1,14 @@
-/*
-The user should run this from the root of the project using `node 1-odd-even/index.js <integer> and should see the word odd or even depending on the integer.  If an integer is not provide, the user should see a usage statement reminding them of the how to run this code.
-*/
 
-// get input using getargs
 let getargs = require("../modules/getargs/index.js")
 let input = getargs.getStringArg()
 
-/**** Do not modify anything above this line ****/
 
-// TODO make sure that the user entered a string
 if (!input) {
 console.log("usage: node 3-string-contains-hello <string>")
 console.log("To include spaces, use quotes.")
 }
 
-// check to see if string contains hello
-// if it does report back the position in the string where it begins
+
 else {
 
   if (input.indexOf('hello') > -1) {
